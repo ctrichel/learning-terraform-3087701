e98d145a143ad4c54157196886e2c5ae07df5330
Christopher Trichel <98132865+ctrichel@users.noreply.github.com>
2024-12-09 16:07:13 -0600

    try more public exposure

--- main.tf
+++ main.tf
@@ -23,6 +23,8 @@ resource "aws_instance" "blog" {
   vpc_security_group_ids = [aws_security_group.blog.id]
   subnet_id = aws_subnet.public_subnet.id
 
+  associate_public_ip_address = true
+
   tags = {
     Name = "tf_class_HelloWorld"
   }
