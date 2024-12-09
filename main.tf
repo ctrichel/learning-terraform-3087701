0d3a87e9090efe1738862e12f2c0addce9c374e8
Christopher Trichel <98132865+ctrichel@users.noreply.github.com>
2024-12-09 16:57:04 -0600

    module for security group

--- main.tf
+++ main.tf
@@ -20,13 +20,29 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
   subnet_id = aws_subnet.public_subnet.id
 
   associate_public_ip_address = true
 
   tags = {
     Name = "tf_class_HelloWorld"
+  }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.2.0"
+  name = "blog_new"
+
+  vpc_id = aws_vpc.tf_class_vpc.id
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
+  tags = {
+    Name = "tf_class_sg"
   }
 }
 
