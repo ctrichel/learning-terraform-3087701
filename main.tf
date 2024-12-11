55e16dc88b3c3b93368011a31bcae8b76ddfef73
Christopher Trichel <98132865+ctrichel@users.noreply.github.com>
2024-12-11 13:43:30 -0600

    using vpc module for dev vpc

--- main.tf
+++ main.tf
@@ -15,27 +15,12 @@ data "aws_ami" "app_ami" {
 }
 
 
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = aws_subnet.public_subnet.id
-
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "tf_class_HelloWorld"
-  }
-}
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
   name = "blog"
 
-  vpc_id = aws_vpc.tf_class_vpc.id
+  vpc_id = module.blog_vpc.vpc_id
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
@@ -46,20 +31,32 @@ module "blog_sg" {
   }
 }
 
-resource "aws_vpc" "tf_class_vpc" {
-  cidr_block = "10.0.0.0/16"
- 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
   tags = {
-    Name = "tf_class_vpc"
+    Name = "tf_class_dev_vpc"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.tf_class_vpc.id
-  cidr_block = "10.0.0.0/24"
-  map_public_ip_on_launch = true
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
+
+  associate_public_ip_address = true
 
   tags = {
-    Name = "tf_class_subnet"
+    Name = "tf_class_HelloWorld"
   }
 }
