75c4764a01251a3902d60f1cf35b600d1c25df6b
Christopher Trichel <98132865+ctrichel@users.noreply.github.com>
2024-12-11 14:26:44 -0600

    using alb module

--- main.tf
+++ main.tf
@@ -60,3 +60,35 @@ resource "aws_instance" "blog" {
     Name = "tf_class_HelloWorld"
   }
 }
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_vpc.security_group_id
+
+  target_groups = {
+    {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  listeners = {
+    ex-tcp-http = {
+      port     = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "tf_class"
+  }
+}
