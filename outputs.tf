d4a954dd088a2c3c5c57ebea43e4fcfc65a1b336
Christopher Trichel <98132865+ctrichel@users.noreply.github.com>
2024-12-11 15:46:49 -0600

    purge invalid outputs file

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,1 @@
-output "instance_ami" {
-  value = aws_instance.blog.ami
-}
 
-output "instance_arn" {
-  value = aws_instance.blog.arn
-}
-
-output "public_ip" {
-  value = aws_instance.blog.public_ip
-}
